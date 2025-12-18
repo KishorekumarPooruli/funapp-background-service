4208292742886d678397ca787ed6f50d64130141
kishorekumar.pooruli@outlook.com <kishorekumar.pooruli@outlook.com>
2025-12-18 09:39:38 +0000

    update

---
+++ TimerTriggerFunctions.cs
@@ -0,0 +1,27 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace funapp_background_service;
+
+public class TimerTriggerFunctions
+{
+    private readonly ILogger _logger;
+
+    public TimerTriggerFunctions(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<TimerTriggerFunctions>();
+    }
+
+    [Function("TimerTriggerFunctions")]
+    public void Run([TimerTrigger("%DailySchedule%")] TimerInfo myTimer)
+    {
+        _logger.LogInformation("" +
+            "C# Timer trigger function executed at: {executionTime}", DateTime.Now);
+
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation("" +
+                "Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
+        }
+    }
+}
